b6c00801ac6e199e58be8b1a4b0b5fd2728db662
Jonathan Conger <jonathan.conger@utah.edu>
2021-11-02 13:09:36 -0600

    android sdk, particle system

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    bool moveAllowed;
+    Collider2D collider;
+    public ParticleSystem water;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<Collider2D>();
+        water.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+		{
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+            switch (touch.phase)
+			{
+                case TouchPhase.Began:
+                    Collider2D touchedCollider = Physics2D.OverlapPoint(touchPosition);
+                    if (collider == touchedCollider)
+                        moveAllowed = true; water.Play();
+                    break;
+                
+                case TouchPhase.Moved:
+                    if (moveAllowed)
+                        transform.position = new Vector2(touchPosition.x, touchPosition.y);
+                    break;
+                
+                case TouchPhase.Ended:
+                    moveAllowed = false; water.Stop();
+                    break;
+			}
+		}
+    }
+}
